52e02fb9259ac0ab8b42f6a80eae8a7cd37cbedd
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-07-10 20:46:39 +0530

    Added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import Movie from './Movie';
 import './App.css'
+import Filter from "./Filter";
 
 function App() {
 
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
 
   useEffect(() => {
     fetchPopular();
@@ -14,12 +17,14 @@ function App() {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=5394a7e19a788439c4e00866a0c2bb6c&language=en-US&page=1');
     const movies = await data.json();
     setPopular(movies.results);
+    setFiltered(movies.results);
   }
 
   return (
     <div className="App">
+     <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre} />
       <div className="popular-movies">
-        {popular.map((movie) => {
+        {filtered.map((movie) => {
           return <Movie key={movie.id} movie={movie} />
         })}
       </div>
